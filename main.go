0d69b532b2b4c6f48d786d29f9104618f1d28d0e
Jake Gallagher <jake.a.gallagher98@gmail.com>
2024-03-22 21:44:48 +0000

    Gin setup

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"example.com/digital-passport/routes"
+	"github.com/gin-gonic/gin"
+)
 
+func main() {
+	server := gin.Default()
+
+	routes.RegisterRoutes(server)
+
+	server.Run(":8080")
 }

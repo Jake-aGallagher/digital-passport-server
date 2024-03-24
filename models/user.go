bc822d5f95990c203cc93d650a6e1d09591543f7
Jake Gallagher <jake.a.gallagher98@gmail.com>
2024-03-24 11:02:08 +0000

    Validate password

--- models/user.go
+++ models/user.go
@@ -1,14 +1,17 @@
 package models
 
 import (
+	"errors"
 	"fmt"
 
 	"example.com/digital-passport/db"
 	"example.com/digital-passport/utils"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type User struct {
+	ID       primitive.ObjectID `bson:"_id,omitempty"`
 	Username string
 	Email    string
 	Password string
@@ -27,4 +30,18 @@ func (u User) Save() (string, error) {
 	}
 	fmt.Println("Inserted a single document from a struct: ", res.InsertedID)
 	return res.InsertedID.(primitive.ObjectID).Hex(), nil
+}
+
+func (u *User) ValidateCredentials() (string, string, error) {
+	var result User
+	err := db.DB.Collection("users").FindOne(db.DBctx, bson.M{"username": u.Username}).Decode(&result)
+	if err != nil {
+		return "", "", errors.New("credentials invalid")
+	}
+
+	passwordValid := utils.CheckPasswordHash(result.Password, u.Password)
+	if !passwordValid {
+		return "", "", errors.New("credentials invalid")
+	}
+	return result.Company, result.ID.Hex(), nil // needs to get user id not username
 }

bc0003baf87718a12a613a57fabdb8de136bfdcc
Jake Gallagher <jake.a.gallagher98@gmail.com>
2024-03-27 21:05:29 +0000

    Authenticate middleware and better CORS

--- routes/routes.go
+++ routes/routes.go
@@ -1,18 +1,24 @@
 package routes
 
 import (
+	"example.com/digital-passport/middlewares"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func RegisterRoutes(server *gin.Engine) {
-	server.Use(cors.Default())
-	server.GET("/passports/:companyid")     // view all passports relevant to a company
-	server.GET("/passports/:companyid/:id") // view a single passport
-	server.POST("/passports")               // creating a brand new passport
-	server.POST("/passport-stages")         // adding a new stage to the passport
-	server.PUT("/passport-stages/:id")      // updating a stage to the passport
-	server.DELETE("passport-stages/:id")    // deleting a stage, only if no stages after this one
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://localhost:3000"}
+	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE"}
+	config.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type", "Authorization"}
+	server.Use(cors.New(config))
+
+	server.GET("/passports/:companyid", middlewares.Authenticate, getPassports) // view all passports relevant to a company
+	server.GET("/passports/:companyid/:id", middlewares.Authenticate)           // view a single passport
+	server.POST("/passports", middlewares.Authenticate, createPassport)         // creating a brand new passport
+	server.POST("/passport-stages", middlewares.Authenticate)                   // adding a new stage to the passport
+	server.PUT("/passport-stages/:id", middlewares.Authenticate)                // updating a stage to the passport
+	server.DELETE("passport-stages/:id", middlewares.Authenticate)              // deleting a stage, only if no stages after this one
 
 	server.POST("/files")
 	server.GET("/files/passport/:id")
